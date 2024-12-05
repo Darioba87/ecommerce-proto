7bd6cee94d5d3693c2c31354920dc5bec1dfe810
Darioba87 <dariobrito87@hotmail.com>
2024-12-05 11:45:27 +0100

    Updates

--- assets/fetchProducts-BA116zsm.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-BoR25OT0.js";import{g as f}from"./productsApi-s99y237h.js";const m=(a="")=>{const[s,r]=t.useState([]),[o,c]=t.useState(!0),[n,u]=t.useState(null);return t.useEffect(()=>{(async()=>{try{const e=await f(a);r(e)}catch(e){u(e.message)}finally{c(!1)}})()},[a]),{data:s,loading:o,error:n}};export{m as u};
